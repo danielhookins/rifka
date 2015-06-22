e01a595fe5c5d4b651e1a8ae936badbe8c112ed6
Daniel Hookins <daniel@hookins.net>
2015-06-22 12:40:52 +0700

    Update CSS styles of Main Search Title and Data Dictionary)

--- resources/views/kamus/index.blade.php
+++ resources/views/kamus/index.blade.php
@@ -1,28 +1,21 @@
-<!-- 
-	TODO: use css instead of inline styling 
--->
 @extends('layouts.master')
 
 @section('nav')
-  <div>
-  	<h3>Tables</h3>
- 
-	  @foreach ($tabletypes as $tabletype)
-	  	<h4>{{ $tabletype->type }}</h4>
-	  	<ul style="list-style-position:inside;padding-left:0;">
-		  @foreach ($tables as $table)
-		  
-		  	@if ($table->type == $tabletype->type)
-		  		<li>
-					  <a href="#{{ $table->name }}">{{ $table->name }}</a>
-					  <!-- {{ $table->description }} -->
-					</li>
-				@endif
-		  @endforeach
-			</ul>
+	<h3>Tables</h3>
+	@foreach ($tabletypes as $tabletype)
+	<div class="panel panel-success">
+  	<div class="panel-heading">
+		<h4 class="panel-title">{{ $tabletype->type }}</h4>
+		</div>
+	  <ul class="list-group">
+	  @foreach ($tables as $table) 
+	  	@if ($table->type == $tabletype->type)
+				  <li class="list-group-item"><a href="#{{ $table->name }}">{{ $table->name }}</a></li>
+			@endif
 	  @endforeach
-  </div>
-
+		</ul>
+	</div>
+  @endforeach
 @endsection
 
 @section('content')
@@ -31,41 +24,45 @@
     <p>This page lists all of the descriptions of the tables and fields in the Rifka Annisa case database.</p>
   </div>
   
-  <!-- Table Start -->
   @foreach ($tables as $table)
-  	<div class="" style="padding:2px 10px 5px 10px;border-style:solid;border-width:1px;margin-bottom:5px;">
-		<h3><a class="in-link" name="{{ $table->name }}">{{ $table->name }}</a></h3>
-		<p>{{ $table->description }}</p>
-		<table class="table table-hover">
-
-	    @foreach ($attributes as $attribute)
-	    	@if($attribute->table == $table->name)
-	    	<!-- Attribute Start -->
-				<tr>
-					<td>
-						<strong>{{ $attribute->name }}</strong>
-						<ul style="list-style-position:inside;padding-left:0;">
-							@if($attribute->primary_key)
-							<li>Primary Key</li>
-							@endif
-							@if($attribute->foreign_key)
-							<li>Foreign Key ({{ $attribute->foreign_key }})</li>
-							@endif
-							<li>{{ $attribute->type }}</li>
-						</ul>
-					</td>
-					<td>
-						{!! $attribute->description !!}
-						<p style="color:blue"><strong>Contoh: </strong>{{ $attribute->example }}</p>
-					</td>
-				</tr>
-				<!-- Attribute End -->
-	    	@endif
-	    @endforeach
-	    <tr><td></td><td class="text-right"><a href="#">Kembali ke atas</a></td></tr>
-		</table>
+		<div class="panel panel-default">
+			
+			<div class="panel-heading">
+				<h3 class="panel-title strongLabel"><a class="in-link" name="{{ $table->name }}">{{ $table->name }}</a></h3>
+			</div>
+			
+			<div class="panel-body">
+				{{ $table->description }}
+			</div>
+			
+			<table class="table table-hover">
+		    @foreach ($attributes as $attribute)
+		    	@if($attribute->table == $table->name)
+					<tr>
+						<td>
+							<strong>{{ $attribute->name }}</strong>
+							<ul>
+								@if($attribute->primary_key)
+								<li>Primary Key</li>
+								@endif
+								@if($attribute->foreign_key)
+								<li>Foreign Key ({{ $attribute->foreign_key }})</li>
+								@endif
+								<li>{{ $attribute->type }}</li>
+							</ul>
+						</td>
+						<td>
+							{!! $attribute->description !!}
+							<p class="kamus-contoh"><strong>Contoh: </strong>{{ $attribute->example }}</p>
+						</td>
+					</tr>
+		    	@endif
+		    @endforeach
+	    </table>
+	    <div class="panel-footer">
+	    	<tr><td colspan=2><a class="btn btn-default" href="#">Kembali ke atas</a></td></tr>
+	    </div>
 	  </div>
-	  <!-- Table End -->
 	@endforeach
 
 @endsection
