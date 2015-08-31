ef7e88f21bd88caf143c7f53da3dd0ebf55eef0f
Daniel Hookins <daniel@hookins.net>
2015-08-31 11:39:31 +0700

    Add validation to new user (request new account) form [basic]
    * Add basic validation check for all registration fields.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -21,31 +21,55 @@
 					<form class="form-horizontal" role="form" method="POST" action="{{ url('/auth/register') }}">
 						<input type="hidden" name="_token" value="{{ csrf_token() }}">
 
-						<div class="form-group">
+						<div class="form-group 
+						@if($errors->has('name')) has-error @endif">
 							<label class="col-md-4 control-label">Nama</label>
 							<div class="col-md-6">
 								<input type="text" class="form-control" name="name" value="{{ old('name') }}">
+								@if($errors->has('name'))
+									<p class="help-block">
+										{{ $errors->first('name') }}
+									</p>
+								@endif
 							</div>
 						</div>
 
-						<div class="form-group">
+						<div class="form-group 
+						@if($errors->has('email')) has-error @endif">
 							<label class="col-md-4 control-label">Alamat E-Mail</label>
 							<div class="col-md-6">
 								<input type="email" class="form-control" name="email" value="{{ old('email') }}">
+								@if($errors->has('email'))
+									<p class="help-block">
+										{{ $errors->first('email') }}
+									</p>
+								@endif
 							</div>
 						</div>
 
-						<div class="form-group">
+						<div class="form-group 
+						@if($errors->has('password')) has-error @endif">
 							<label class="col-md-4 control-label">Password</label>
 							<div class="col-md-6">
 								<input type="password" class="form-control" name="password">
+								@if($errors->has('password'))
+									<p class="help-block">
+										{{ $errors->first('password') }}
+									</p>
+								@endif
 							</div>
 						</div>
 
-						<div class="form-group">
+						<div class="form-group 
+						@if($errors->has('password_confirmation')) has-error @endif">
 							<label class="col-md-4 control-label">Confirmasi Password</label>
 							<div class="col-md-6">
 								<input type="password" class="form-control" name="password_confirmation">
+								@if($errors->has('password_confirmation'))
+									<p class="help-block">
+										{{ $errors->first('password_confirmation') }}
+									</p>
+								@endif
 							</div>
 						</div>
 
