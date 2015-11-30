1fbeb93b4cb475b3ffa7458cbffaf97804055826
Daniel Hookins <daniel@hookins.net>
2015-11-30 10:57:31 +0700

    Revert spelling from last commit
    * Oops, turns out it was already correct, the DB entries are wrong.

--- resources/views/kasus/partials/dampak-edit.blade.php
+++ resources/views/kasus/partials/dampak-edit.blade.php
@@ -23,7 +23,7 @@
           {!! Form::select('jenis_dampak', array(
             'Dampak' =>  'Dampak',
             'Kekerasan Fisik' => 'Kekerasan Fisik',
-            'Kekerasan Jiwa' => 'Kekerasan Jiwa',
+            'Kesehatan Jiwa' => 'Kesehatan Jiwa',
             'Perilaku tidak sehat' => 'Perilaku tidak sehat',
             'Kesehatan reproduksi' => 'Kesehatan reproduksi',
             'Kondisi klinis' => 'Kondisi klinis',
