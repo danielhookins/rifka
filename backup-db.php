1e2b04c0782d2b673d223232383622730ccf1b8b
Daniel Hookins <daniel@hookins.net>
2016-02-15 11:12:58 +0700

    Update DB Backup script to only keep two backup files at a time.
    * bak.sql most recent db dump
    * bak.old the previous db dump

--- backup-db.php
+++ backup-db.php
@@ -17,10 +17,27 @@
 	$dbPassword 	= Dotenv::findEnvironmentVariable('DB_PASSWORD');
 	$dbBackupPath = Dotenv::findEnvironmentVariable('DB_BACKUP_PATH');
 	
-	// Create the mysqldump command
-	$mysqlDump = "mysqldump -h ".$dbHost." -u ".$dbUsername." -p".$dbPassword." ".$dbDatabase." > ".$dbBackupPath."bak".date("U").".sql";
+	try {
+		// Rename existing backup file
+		if(file_exists($dbBackupPath."bak.sql"))
+		{
+			// Delete existing old backup file
+			if(file_exists($dbBackupPath."bak.old"))
+			{
+				unlink($dbBackupPath."bak.old");
+			}
+			rename($dbBackupPath."bak.sql", $dbBackupPath."bak.old");
+		}
 
-	// Execute the command
-	exec($mysqlDump);
+		// Create the mysqldump command
+		$mysqlDump = "mysqldump -h ".$dbHost." -u ".$dbUsername." -p".$dbPassword." ".$dbDatabase." > ".$dbBackupPath."bak.sql";
+
+		// Execute the command
+		exec($mysqlDump);
+
+	} catch (Exception $e) {
+		echo "Error: Could not back up database.\n";
+		echo $e;
+	}	
 
 ?>
