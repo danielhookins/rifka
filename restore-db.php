a0b59749d6b2c48844cb3cf74ca62166d28e96f6
Daniel Hookins <daniel@hookins.net>
2016-02-15 11:52:20 +0700

    Add script that will recover DB from backup file

---
+++ restore-db.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Restore DB from backup file
+ */ 
+
+	// Load .env file
+	require_once __DIR__.'/bootstrap/autoload.php'; 
+	Dotenv::load(__DIR__);
+	
+	// Set environment variables
+	$dbHost 			= Dotenv::findEnvironmentVariable('DB_HOST');
+	$dbDatabase 	= Dotenv::findEnvironmentVariable('DB_DATABASE');
+	$dbUsername 	= Dotenv::findEnvironmentVariable('DB_USERNAME');
+	$dbPassword 	= Dotenv::findEnvironmentVariable('DB_PASSWORD');
+	$dbBackupPath = Dotenv::findEnvironmentVariable('DB_BACKUP_PATH');
+
+	try {
+		
+		// Restore from existing backup file
+		if(file_exists($dbBackupPath."bak.sql"))
+		{
+			// Create the mysql command
+			$mysqlCmd = "mysql -h ".$dbHost." -D ".$dbDatabase." -u ".$dbUsername." -p".$dbPassword." < ".$dbBackupPath."bak.sql";
+
+			// Execute the command
+			exec($mysqlCmd);
+			
+		} else {
+			echo "Error: could not restore database.\n";
+			echo "No backup file exists.";
+		}
+
+	} catch (Exception $e) {
+		echo "Error: Could not restore database.\n";
+		echo $e;
+	}	
+
+?>
