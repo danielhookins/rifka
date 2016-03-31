deafbdff8c856f5e18935170de55c7907cf33182
Daniel Hookins <daniel@hookins.net>
2016-03-31 11:35:32 +0000

    Add bentuk kekerasan report

--- resources/views/layouts/partials/nav-Konselor.blade.php
+++ resources/views/layouts/partials/nav-Konselor.blade.php
@@ -19,4 +19,11 @@
 <li>
   <a href="{{ route('konselor.index') }}">Konselor</a>
 </li>
-<li><a href="{{ route('laporan.index') }}">Laporan</a></li>
+
+<li class="dropdown">
+  <a href="{{ route('laporan.index') }}" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Laporan <span class="caret"></span></a>
+  <ul class="dropdown-menu" role="menu">
+    <li><a href="{{ route('laporan.index') }}">Membuat Laporan</a></li>
+    <li><a href="{{ route('laporan.bentukkekerasan') }}">Bentuk Kekerasan</a></li>
+  </ul>
+</li>
